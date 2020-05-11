f48626a6cd96ba2740b2ab28287d56b1f5a3d49b
John Louis Rumingan <jmrumingan@gmail.com>
2020-05-11 12:29:41 -0400

    Add collatz_problem

---
+++ lib/collatz_problem.rb
@@ -0,0 +1,25 @@
+
+# returns next even number input for n
+def even_next(n)
+
+end
+
+# return next odd number input for n
+def odd_next(n)
+
+end 
+
+#returns next value in a sequence for any interget input of n
+def next_value(n)
+
+end
+
+#returns Collatz sequence from n to 1 in an array
+def collatz(n)
+
+end
+
+#returns starting number under one million that returns the longest sequence
+def longest_collatz(n)
+
+end 
