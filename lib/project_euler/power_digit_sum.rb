464915668d4a40480df8f688cfb4c1348f20afcd
John Louis Rumingan <jmrumingan@gmail.com>
2020-05-13 10:05:54 -0400

    Complete power digit sum. All rspec tests pass.

--- lib/project_euler/power_digit_sum.rb
+++ lib/project_euler/power_digit_sum.rb
@@ -1,5 +1,12 @@
-
 #base x, exponent n
-def power_digit_sum (x, n)
+def power_digit_sum (x = 0, n = 0)
 
+    target = x ** n 
+    #split target into array of string characters, then collect characters and add them
+    sum = 0
+    target = target.to_s.split('')
+    target.each do |num|
+        sum += num.to_i
+    end 
+    return sum 
 end 
