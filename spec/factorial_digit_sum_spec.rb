ad1da22e16222258731d76a933057f2f337eb1fc
John Louis Rumingan <jmrumingan@gmail.com>
2020-05-14 12:02:48 -0400

    Add factorial digit sum test

---
+++ spec/factorial_digit_sum_spec.rb
@@ -0,0 +1,7 @@
+
+describe "#factorial_digit_sum" do 
+
+    it "takes a number (n) as an argument and returns the sum of the digits of n!" do
+        expect(factorial_digit_sum(4)).to eq(6)
+    end
+end 
