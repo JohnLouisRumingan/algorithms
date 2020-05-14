d7bf17a36bf35bd0117c3d4ab5b97d20227fee1a
John Louis Rumingan <jmrumingan@gmail.com>
2020-05-14 12:08:53 -0400

    Add 2 more factorial digit sum tests

--- spec/factorial_digit_sum_spec.rb
+++ spec/factorial_digit_sum_spec.rb
@@ -5,3 +5,17 @@ describe "#factorial_digit_sum" do
         expect(factorial_digit_sum(4)).to eq(6)
     end
 end 
+
+describe "#sum_of_digits" do
+
+    it "takes a number (n) as an argument and returns the sum of its digits" do 
+        expect(sum_of_digits(24)).to eq(6)
+    end 
+end 
+
+describe "#factorial" do 
+
+    it "takes a number(n) as an argument and returns the factorial of (n)" do 
+        expect
+    end 
+end 
